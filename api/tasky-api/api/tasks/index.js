2e957c106bbfac1f76c3dcc9475fe5c48a6d1179
zbrennan5642 <zbrennan5642@gmail.com>
2025-12-03 11:59:07 +0000

    Mongo lab Error Handling

--- api/tasky-api/api/tasks/index.js
+++ api/tasky-api/api/tasks/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Task from './taskModel';
+import asyncHandler from 'express-async-handler';
 import '../../db';
 
 const router = express.Router(); // eslint-disable-line
@@ -16,30 +17,9 @@ router.post('/', async (req, res) => {
     res.status(201).json(task);
 });
 
-// Update Task
-router.put('/:id', async (req, res) => {
-    if (req.body._id) delete req.body._id;
-    const result = await Task.updateOne({
-        _id: req.params.id,
-    }, req.body);
-    if (result.matchedCount) {
-        res.status(200).json({ code:200, msg: 'Task Updated Successfully' });
-    } else {
-        res.status(404).json({ code: 404, msg: 'Unable to find Task' });
-    }
-});
-
-// delete Task
-router.delete('/:id', async (req, res) => {
-    const result = await Task.deleteOne({
-        _id: req.params.id,
-    });
-    if (result.deletedCount) {
-        res.status(204).json();
-    } else {
-        res.status(404).json({ code: 404, msg: 'Unable to find Task' });
-    }
-});
-
+router.post('/', asyncHandler(async (req, res) => {
+    const task = await Task(req.body).save();
+    res.status(201).json(task);
+}));
 
 export default router;
