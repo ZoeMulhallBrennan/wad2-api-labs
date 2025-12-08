d57d9730b234af1682f42267b247cfb26fe130aa
zbrennan5642 <zbrennan5642@gmail.com>
2025-12-08 13:03:48 +0000

    Added Auth Middleware

--- api/tasky-api/index.js
+++ api/tasky-api/index.js
@@ -5,6 +5,7 @@ import tasksRouter from './api/tasks';
 import cors from 'cors';
 //... other imports
 import usersRouter from './api/users';
+import authenticate from './authenticate';
 
 
 dotenv.config();
@@ -25,7 +26,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use('/api/tasks', tasksRouter);
+app.use('/api/tasks', authenticate, tasksRouter);
 
 app.use(errHandler);
 
